d9d34d946d0ee6708f600a9a5787066d34819cb5
maxull <max.ull@online.no>
2022-10-13 19:59:55 +0200

    global test data prepp

--- Analysis base code/global test - ebGSEA.R
+++ Analysis base code/global test - ebGSEA.R
@@ -86,10 +86,11 @@ CGtoGENE2 <- CGtoGENE2 %>%
         filter(!ENTREZ == "NULL") %>% 
         select(2)
 
-uniquecg <- as.data.frame(unique(CGtoGENE2$ENTREZ))    ### 23329 unique entrez IDs instead of 16k from annEPIC file
+uniquecg <- as.data.frame(unique(CGtoGENE2$ENTREZ)) %>% 
+        na.omit()  ### 23329 unique entrez IDs instead of 16k from annEPIC file
 
-uniquecg <- as.data.frame(unique(probe.features$gene)) ### 26650 total unique gene ids in probe features
-uniquecg <- as.data.frame(unique(annEPIC$UCSC_RefGene_Name)) ### 66088 here
+#uniquegene <- as.data.frame(unique(probe.features$gene)) ### 26650 total unique gene ids in probe features
+#uniquegene2 <- as.data.frame(unique(annEPIC$UCSC_RefGene_Name)) ### 66088 here
 
 ###
 
@@ -100,6 +101,82 @@ nrep.v <- unlist(lapply(subsets,length))
 print(nrep.v)
 
 summarise(d)
+
+################3
+### custom large subset list generation for global test
+
+a <- uniquecg[1,]
+list(rownam)
+
+f_bVals %>% 
+        rownames_to_column() -> df
+
+gene1 <- CGtoGENE2 %>% 
+        filter(ENTREZ == a) %>% 
+        rownames_to_column()
+
+left_join(gene1, df, by = "rowname") -> g1
+
+
+g1 %>% 
+        na.omit() -> g1  
+        g1[,-1:-2] -> g1
+        
+rownames(g1) <- g1[,1] 
+g1[,-1]
+
+g1 = as.data.frame(t(g1))
+
+d <- rownames_to_column(d, var = "colnames")
+
+
+################### trying global test
+library(globaltest);library(AnnotationDbi)
+
+gt(response = pd_gt, alternative = ~"Baseline" +"7wk_Loading", data = d)
+
+###
+#change pd1 to only contain rownames and group
+pd1 %>% 
+        select(colnames, Sample_Group) -> pd_gt
+
+rownames(pd_gt) <- pd_gt[,1]
+
+pd_gt <- pd_gt[,-1]
+
+
+
+
+d <- as.data.frame(d)
+
+
+
+d <- t(g1)
+
+
+
+d2 <- left_join(d, pd_gt, by = "colnames")
+
+d2 %>% 
+        aov(cg00000029  ~Sample_Group, data = .) %>% 
+        summary()
+
+
+
+
+cpgs <-rownames(g1)
+
+
+
+d3 <- d2[,-1]
+
+lm(.~as.factor(Sample_Group), data = d3)
+anova(mod)
+
+
+class(d3$Sample_Group)
+
+d3$Sample_Group <- as.factor(d3$Sample_Group)
 
 
 
