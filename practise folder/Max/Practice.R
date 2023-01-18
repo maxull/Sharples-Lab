81d9c8a9aa1794cda856194d022b9f2bbcde15aa
Max <max.ull@online.no>
2023-01-18 21:30:34 +0100

    figuring out the mac OS

--- practise folder/Max/Practice.R
+++ practise folder/Max/Practice.R
@@ -1,1 +1,13 @@
 # this will be first practise script for first basic session i R
+
+library(ChAMP) # error
+library(tidyverse)
+library(ggplot2)
+library(readxl)
+
+
+getwd()
+
+# example: not tidy dataformat
+df <- read_excel("/Users/danielturner/Dropbox/NIH/Study 1 - Human Repeated Muscle Atrophy (RMA)/Data/DXA/RMA_003/DXA Results.xlsx")
+
