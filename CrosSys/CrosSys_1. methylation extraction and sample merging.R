73fc2b02e2bf8cd8613bce2904676415fd85f5d7
Max Ullrich <max.ull@online.no>
2024-07-06 12:55:35 +0200

    CrosSys sample processing

---
+++ CrosSys/CrosSys_1. methylation extraction and sample merging.R
@@ -0,0 +1,182 @@
+#
+#               CrosSys
+#
+#               Load and merge data
+#
+
+# BiocManager::install('methylKit')
+
+# Load packages
+library(methylKit)
+library(tidyverse)
+library(ggplot2)
+library(parallel)
+library(pbapply)
+
+# Set working directory
+setwd("/Users/maxullrich/OneDrive - UGent/CrosSys/")
+
+
+# load and combine all three files, filter for mincov = 10, and then save on harddisk. Remove file from R, and continue to next file pair. 
+
+
+
+##########################################################################################################
+#########               get all files                          ###########################################
+##########################################################################################################
+
+
+# Locate all files
+meth_files_1 <- as.list(list.files(pattern = '*.cov.gz', 
+                                      path = "./methylation_results/methylation_results_1st_seq", full.names = TRUE))
+
+# remove nedativa and positive control
+meth_files_1 <- meth_files_1[1:135]
+
+meth_files_2 <- as.list(list.files(pattern = '*.cov.gz', 
+                                   path = "./methylation_results/methylation_results_2nd_seq", full.names = TRUE))
+
+meth_files <- c(meth_files_1, meth_files_2)
+
+
+##########################################################################################################
+#########               identify sample name                   ###########################################
+##########################################################################################################
+
+# get the list of unique file names
+
+sample_names <- sub(".*220048-II-(.*?)_S.*", "\\1", unlist(meth_files)) %>% unique()
+
+
+
+##########################################################################################################
+#########               loop that:                                                      ##################
+#########               1. identifies matching samples                                  ##################
+#########               2. gets the data                                                ##################
+#########               3. merges and filters for minimum coverage of 10                ##################
+#########               4. saves the merged filtered file in folder "./merged_meth_files/"   ##################
+##########################################################################################################
+
+i =3
+
+treatment <- c()
+
+treatment[grep('PRE-ASAT', meth_files)]  <- 0
+treatment[grep('POST-ASAT', meth_files)] <- 1
+treatment[grep('PRE-QF', meth_files)]    <- 2
+treatment[grep('POST-QF', meth_files)]   <- 3
+
+
+for (i in 1:length(sample_names)) {
+        
+        index_1 <- grep(sample_names[i], meth_files)
+        
+        # get the file paths
+        
+        sample_files <- c(meth_files[index_1])
+        
+
+        # Read in data using methRead
+        sample_data <- methRead(sample_files,
+                                sample.id = as.list(sample_files),
+                                assembly = 'hg38',
+                                pipeline = 'bismarkCoverage',
+                                header = F,
+                                treatment = treatment[index_1],
+                                mincov = 1)
+        
+        print(paste("Read data for sample:", sample_names[i]))
+        
+        # merge the files together
+        
+        # Initialize merged_data with the first data set
+        merged_data <- getData(sample_data[[1]])
+        
+        # Loop through the rest of the sample_data starting from the second file
+        for (n in 2:length(sample_data)) {
+                merged_data <- merge(merged_data,
+                                     getData(sample_data[[n]]),
+                                     by = c("chr", "start", "end", "strand"),
+                                     all = TRUE)
+                
+        }
+        
+        print(paste("Merged data for sample:", sample_names[i]))
+        
+        
+        # Replace NA with 0 in coverage, numCs, and numTs columns
+        merged_data <- merged_data %>%
+                mutate(across(c(coverage, numCs, numTs), ~if_else(is.na(.), 0, .))) %>%
+                mutate(coverage = rowSums(select(., starts_with("coverage")), na.rm = TRUE),
+                       numCs = rowSums(select(., starts_with("numCs")), na.rm = TRUE),
+                       numTs = rowSums(select(., starts_with("numTs")), na.rm = TRUE)) %>%
+                select(chr, start, end, strand, coverage, numCs, numTs) %>%
+                filter(coverage > 9) %>% 
+                as.data.frame()
+        
+        
+        saveRDS(merged_data, paste0("./methylation_results/merged_meth_files/", sample_names[i], ".RDATA"))
+        
+        print(paste("Finished with file", i, "/", length(sample_names), " filename:", sample_names[i]))
+        
+}
+
+
+###
+#       Parallel version
+###
+
+process_sample <- function(i) {
+        index_1 <- grep(sample_names[i], meth_files)
+        
+        # Get the file paths
+        sample_files <- c(meth_files[index_1])
+        
+        # Read in data using methRead
+        sample_data <- methRead(sample_files,
+                                sample.id = as.list(sample_files),
+                                assembly = 'hg38',
+                                pipeline = 'bismarkCoverage',
+                                header = FALSE,
+                                treatment = treatment[index_1],
+                                mincov = 1)
+        
+
+        # Merge the files together
+        merged_data <- getData(sample_data[[1]])
+        
+        for (n in 2:length(sample_data)) {
+                merged_data <- merge(merged_data,
+                                     getData(sample_data[[n]]),
+                                     by = c("chr", "start", "end", "strand"),
+                                     all = TRUE)
+        }
+        
+
+        # Replace NA with 0 in coverage, numCs, and numTs columns
+        merged_data <- merged_data %>%
+                mutate(across(c(coverage, numCs, numTs), ~if_else(is.na(.), 0, .))) %>%
+                mutate(coverage = rowSums(select(., starts_with("coverage")), na.rm = TRUE),
+                       numCs = rowSums(select(., starts_with("numCs")), na.rm = TRUE),
+                       numTs = rowSums(select(., starts_with("numTs")), na.rm = TRUE)) %>%
+                select(chr, start, end, strand, coverage, numCs, numTs) %>%
+                filter(coverage > 9) %>%
+                as.data.frame()
+        
+        saveRDS(merged_data, paste0("./methylation_results/merged_meth_files/", sample_names[i], ".RDATA"))
+        
+
+        return(merged_data)
+}
+
+# Detect the number of available cores
+num_cores <- detectCores() - 1  # Leave one core free
+
+# Parallel processing with progress bar
+result <- pblapply(1:length(sample_names), process_sample, cl = num_cores)
+
+
+
+
+
+
